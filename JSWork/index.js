290687e40f7491294f386c7d140c082ed6960f5c
aryanbansal29 <baryan740@gmail.com>
2024-10-18 13:07:04 +0530

    complete work

---
+++ JSWork/index.js
@@ -0,0 +1,15 @@
+// console.log("hello world");
+
+// const a=12;
+// if(a>10){
+//     let b=20;
+//     a=40;
+//     console.log("a inside:"+a);
+// }
+// console.log("a outside:"+b);
+function fun(a,b){
+    console.log("hiiii");
+    return a+b;
+}
+const result=fun();
+console.log(result);
