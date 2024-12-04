ba8da0f0e623b87c3dac11bae47049c422369cce
aryanbansal29 <baryan740@gmail.com>
2024-12-04 13:09:11 +0530

    introduction of react

---
+++ reactwork/reactviteapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Student from './Student' 
+
+function App() {
+
+
+  return (
+    <div>
+      <div>
+        <h2 style={{color:'red'}}>Hello React JS</h2>
+        <div>
+          <Student  name="Aryan Bansal" roll="1234" college="ABES Engineering College" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
